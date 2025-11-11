7a1703ae465d89b8f48891365af1dd6823e8a2fb
ernazarovogabek <ernazarovogabek278@gmail.com>
2025-11-11 16:59:40 +0500

    gewivrhihjo

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,9 @@ import React from 'react'
 
 const App = () => {
   return (
-    <div className='bg-red-600'>App</div>
+    <div className='bg-red-600'>
+      <h1>Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat quasi earum obcaecati beatae consectetur illo voluptates ipsam deleniti sapiente vero officia similique sint accusamus cupiditate tenetur sit, nobis ex doloremque.</h1>
+    </div>
   )
 }
 
