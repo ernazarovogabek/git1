bd656df629eccc6010aa4e43d109ff7e38ce5563
ernazarovogabek <ernazarovogabek278@gmail.com>
2025-11-11 17:07:04 +0500

    hvdefruhieh

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
+import Se from './assets/Se.png'
 
 const App = () => {
   return (
     <div className='bg-red-600'>
       <h1>Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat quasi earum obcaecati beatae consectetur illo voluptates ipsam deleniti sapiente vero officia similique sint accusamus cupiditate tenetur sit, nobis ex doloremque.</h1>
+      <img src={Se} alt="" />
     </div>
   )
 }
